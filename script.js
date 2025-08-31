733a00715ee5706349441ef94029d73d9a52c2d1
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-31 09:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+  
+  let total = 0;
+  prices.forEach(price => {
+    total += parseFloat(price.textContent) || 0; // convert to number
+  });
+
+  // Display the total
+  const totalDiv = document.createElement("div");
+  totalDiv.textContent = `Total Price: ${total}`;
+  document.body.appendChild(totalDiv);
+	
   
 };
 
